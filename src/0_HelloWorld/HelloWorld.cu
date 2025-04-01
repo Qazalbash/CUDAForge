9c3e62553eb84de4eee09371b61a1850e341373d
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-04-02 04:03:41 +0500

    refactor: rename GPU kernel function for clarity

--- src/0_HelloWorld/HelloWorld.cu
+++ src/0_HelloWorld/HelloWorld.cu
@@ -1,11 +1,11 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-__global__ void HelloWorldKernel() { printf("Hello World from GPU (device)\n"); }
+__global__ void helloFromGPU() { printf("Hello World from GPU (device)\n"); }
 
 int main(void) {
     printf("Hello from CPU (host) before kernel execution\n");
-    HelloWorldKernel<<<1, 32>>>();
+    helloFromGPU<<<1, 32>>>();
     cudaDeviceSynchronize();
     printf("Hello from CPU (host) after kernel execution\n");
 
